7ce5b94d1c6b2dbd91d6cc792ae38e97f61e0c10
kateYoung39 <kjyz76@mail.umkc.edu>
2015-05-24 15:04:52 -0500

    Update header CSS

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,11 @@
 
 @section('content')
 
-<h1>About</h1>
+<h1>Who Are We?</h1>
 
-<p>Meow</p>
+<p>The UMKC Mock Trial Team is a student organization dedicated to competing in intercollegiate tournaments across the
+    United States. In these competitions, student competitors put on imitation trials as either the prosecution
+    or defense team. Competitors demonstrate their command of the court room by playing both the roles of witnesses
+    and attorneys in a realistic trial setting.
+</p>
 @stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,10 @@
 @extends('app')
 @section('content')
-    <h1>Home</h1>
+    <div class="jumbotron">
+        <h1>Interested In Joining?</h1>
+        <p>We are currently looking for dedicated mockers for our 2015-2016 season.</p>
+        <p>Want to join but don't have any experience? No Problem! Everything you need to know will
+            be taught to you by the coaches before the first competition.</p>
+        <p><a class="btn btn-primary btn-lg" href="about" role="button">Learn more</a></p>
+    </div>
 @stop

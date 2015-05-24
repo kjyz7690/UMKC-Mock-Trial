4f9c237fe04045d78ab779ca0908369a274ed3c5
kateYoung39 <kjyz76@mail.umkc.edu>
2015-05-23 21:36:42 -0500

    Update form and add year in school

---
+++ database/migrations/2015_05_24_020242_add_year_to_interested_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddYearToInterestedTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('interested', function(Blueprint $table)
+		{
+            $table->string('year');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('interested', function(Blueprint $table)
+		{
+            Schema::drop('interested');
+		});
+	}
+
+}
--- resources/views/pages/join.blade.php
+++ resources/views/pages/join.blade.php
@@ -2,10 +2,34 @@
 
 @section('content')
 	<h1>Join</h1>
-	<div class="input-group">
-		<input type="text" class="form-control" placeholder="Name">
-	</div>
-	<div class="input-group">
-		<input type="text" class="formcontrol" placeholder="Email">
-	</div>
+    <p>We are excited that you are interested in trying out for the UMKC Mock Trial team! Please enter your contact
+    information below and a current member will get in contact with you with shortly.</p>
+    <form>
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" id="name" class="form-control" placeholder="Name">
+        </div>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="text" id="email" class="form-control" placeholder="Email">
+        </div>
+        <div class="form-group">
+            <label for="phone">Phone Number:</label>
+            <input type="tel" id="phone" class="form-control" placeholder="Phone Number">
+        </div>
+        <div class="form-group">
+        <p>Year in School(as of fall 2015):</p>
+            <select class="form-control" id="year">
+                <option id="fresh">Freshman</option>
+                <option id="soph">Sophomore</option>
+                <option id="jun">Junior</option>
+                <option id="sen">Senior</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="comment">Comments:</label>
+            <textarea class="form-control" rows="5" id="comment"></textarea>
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+    </form>
 @stop
